72a53e7319c84b428a185e8978e4950d04169b07
Manviksagar <aallu.vidyasagar@gmail.com>
2021-04-18 20:20:22 +0530

    Selenium4 TAB feature

--- src/test/java/sele4/Sele4NewTab.java
+++ src/test/java/sele4/Sele4NewTab.java
@@ -7,21 +7,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Sele4NewTab {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+
+        WebDriverManager.chromedriver().setup();
+          driver = new ChromeDriver();
+    }
 
 
     @Test
     public void test1(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+
         //driver.findElement(By.id()).sendKeys();
         driver.switchTo().newWindow(WindowType.WINDOW);
         driver.get("https://www.google.com/");
 
 
 
+    }
+    @Test
+    public void test2(){
+
+        driver.switchTo().newWindow(WindowType.TAB);
+        driver.get("https://www.google.com/");
     }
 
 }
