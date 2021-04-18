6ac02ddafc7b59b2339c6061b2dbe5fd462d4f38
Manviksagar <aallu.vidyasagar@gmail.com>
2021-04-18 21:33:56 +0530

    Relative Tagnames added

--- src/test/java/sele4/Sele4NewTab.java
+++ src/test/java/sele4/Sele4NewTab.java
@@ -6,6 +6,8 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -47,6 +49,22 @@ public class Sele4NewTab {
         driver.manage().window().maximize();
         File srcfile=driver.findElement(By.xpath("//div[@id='divLogo']//img")).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(srcfile, new File("logo.png"));
+    }
+
+    //Testing relativeTags in selenium4
+
+    @Test
+    public void relTags(){
+        driver.get("https://automationbookstore.dev/");
+        WebElement pid5=driver.findElement(RelativeLocator.withTagName("li").toLeftOf(By.id("pid6")).below(By.id("pid1")));
+        String id=pid5.getAttribute("id");
+        System.out.println(id);
+    }
+
+    @AfterTest
+    public void tearDown(){
+
+        driver.quit();
     }
 
 }
